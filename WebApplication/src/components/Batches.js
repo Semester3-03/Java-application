36552b7d60a8c90ae373a86c18de9a5d96c5c917
Teyson <teis@aalbaek-nielsen.dk>
2020-11-20 13:31:53 +0100

    Fix response read
    The response was made into a JSON before we were ready for that.

--- WebApplication/src/components/Batches.js
+++ WebApplication/src/components/Batches.js
@@ -11,22 +11,28 @@ export class Batches extends Component {
 
     search = (e) => {
         fetch('http://localhost:8080/api/batches/' + document.getElementById("searchField").value)
-        .then(response => response.json())    
-        .then(data => {
-                if (data.status === 200) {
-                    let body = JSON.parse(data.body);
-                    this.setState({ selectedBatchID: body.respose, selectSuccess: true, errorMessage: "" });
-                } else if (data.status === 400) {
-                    this.setState({ selectedBatchID: "", selectSuccess: false, errorMessage: "Something went wrong, have you entered a valid UUID?" })
-                } else {
-                    //REBASE AND CHANGE RESPOSE TO RESPONSE
-                    this.setState({ selectedBatchID: "", selectSuccess: false, errorMessage: data.respose});
-                }
-            })
+            .then(response => {
+                let json = response.json();
+                json.then(data => {
+                    if (response.status === 200) {
+                        console.log("Inside 200 ok if");
+                        this.setState({ selectedBatchID: data.id, selectSuccess: true, errorMessage: "" });
+                    } else if (response.status === 400) {
+                        this.setState({ selectedBatchID: "", selectSuccess: false, errorMessage: "Something went wrong, have you entered a valid UUID?" })
+                    } else {
+                        console.log(data.response)
+                        this.setState({ selectedBatchID: "", selectSuccess: false, errorMessage: data.response });
+                    }
+                })
+            }
+            )
+
     }
 
     generatePDF = (e) => {
         fetch('http://localhost:8080/api/batches/' + this.state.selectedBatchID + '/generate')
+        //.then(response => response.json())
+        //.then(data => console.log(data))
     }
 
     render() {
@@ -38,7 +44,7 @@ export class Batches extends Component {
             errorMessage = <p></p>
         } else {
             selectedBatchMessage = <p>Selected batch: none</p>
-            errorMessage = <p style={{color: "red"}}>{this.state.errorMessage}</p>
+            errorMessage = <p style={{ color: "red" }}>{this.state.errorMessage}</p>
         }
 
         return (
@@ -54,7 +60,7 @@ export class Batches extends Component {
                     <button onClick={this.search}>Search</button>
                 </div>
                 <div>
-                    <button onClick={this.generatePDF}>Generate PDF Report</button>
+                    {this.state.selectSuccess === true ? (<button onClick={this.generatePDF}>Generate PDF Report</button>) : (<p></p>)}
                 </div>
 
             </div>
