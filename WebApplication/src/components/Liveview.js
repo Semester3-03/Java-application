9769d9ec9ec5e3c685cf267f5592e175a3cd078d
Christopher <christopher@kas.net>
2020-11-25 23:56:03 +0100

    Add comments and store data as json

--- WebApplication/src/components/Liveview.js
+++ WebApplication/src/components/Liveview.js
@@ -7,49 +7,56 @@ export class Liveview extends Component {
     state = { 
         machineID: "",
         socket: null,
-        stompClient: null
+        stompClient: null, 
+        livedata: {}
     };
 
     componentDidMount() {
+        //Initialize socket and stomp client.
         let socket = new SockJS('http://localhost:8080/websocket')
         let stompClient = Stomp.over(socket);
 
+        //Save machineID -> then socket -> then stomp client -> then connect
         this.setState({machineID: this.props.currentMachine.id}, () => 
             this.setState({socket: socket}, () => 
                 this.setState({stompClient: stompClient}, () => 
-                    this.connect(this.state.machineID, this.testFunction)
+                    this.connect(this.state.machineID, this.storeData)
                 )
             )
         );
     }
 
+    //Connect method that subscribes to the live data
     connect(id, func) {
         let stompClient = this.state.stompClient
+        //Start connection
         stompClient.connect({}, function (frame) {
-            console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/' + id + '/livedata', function (greeting) {
-                //this.getLiveData(JSON.parse(greeting.body).content, id)
-                func(greeting.body)
+            //Send initial info to backend to start the sending of data
+            stompClient.send("/app/connect/" + id, {}, JSON.stringify({'name': "filler value"})); 
+            //Start subscription (when we get data we send random data which triggers the backend to send data)
+            //Essentially we just loop through this forever.
+            stompClient.subscribe('/topic/' + id + '/livedata', function (data) {
+                //Send new data to function storeData
+                func(data.body)
+                //Send random info to backend to "trigger" it to send a few set of data.
                 stompClient.send("/app/connect/" + id, {}, JSON.stringify({'name': "filler value"})); 
             });
         });
     }
 
-    testFunction = (data) => {
-        console.log("I TESTFUNCTION:", data)
-    }
-
-    buttonPress = (e) => {
-        this.state.stompClient.send("/app/connect/" + this.state.machineID, {}, JSON.stringify({'name': "filler value"})); 
+    //Convert data to json and store it in the state variable
+    storeData = (data) => {
+        let json = JSON.parse(data);
+        this.setState({livedata: json});
     }
 
     render() {
         return (
+            // TODO: ADD LIVEDATA VIEW HERE.
+            // DATA CAN BE FOUND IN THE VARIABLE STATE.
+            // GET IT WITH this.state.livedata.{variablename}
             <div>
-                <p>asd</p>
-                <button onClick={this.buttonPress}>
-                    Yoooooooo
-                </button>
+               
             </div>
         )
     }
